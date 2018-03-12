6303f67c952f067ef381c927d6b63fddd17d2ad4
jchristens <jens.christens@live.com>
2018-03-12 16:21:20 +0100

    Add files via upload

---
+++ PSModule-WLANManager.psm1
@@ -0,0 +1,568 @@
+﻿################
+# WLAN Manager #
+################
+#Version: 2015-05-07
+#Author: johan.carlsson@innovatum.se , http://www.innovatum.se/personer/johan-carlsson
+
+## Version 12/03/2018
+## Updated by JENS CHRISTENS - jens.christens@live.com
+
+## Changelog:
+## - Fixed WLANAdapters WMI Query
+## - Fixed WLANAdapters Disable Issues (With Wifi MiniPort)
+## - Updated Write-Hosts for Write-Outputs
+## - Added some troubleshooting hints
+
+#Collect OS Information
+$Global:Win8orGreater = [Environment]::OSVersion.Version -ge (new-object 'Version' 8,0)
+
+##################################
+# Function: Disable-WLANAdapters #
+##################################
+
+function Disable-WLANAdapters
+{
+    #Get only WLAN Adapters
+    $WLANAdapters = Get-WmiObject -Class Win32_NetworkAdapter | Where-Object {($_.Description -notlike “*VirtualBox*”) -and ($_.Description -notlike “*VMware*”) -and ($_.Description -like “*Wireless*”) -or ($_.Description -like “*Wi-Fi*”)}
+    foreach ($WLANAdapter in $WLANAdapters)
+        {
+            #Disable WLAN Adapter
+            $WLANAdapter.Disable()
+        }
+}
+
+
+#################################
+# Function: Enable-WLANAdapters #
+#################################
+
+function Enable-WLANAdapters
+{
+    #Get only WLAN Adapter
+    $WLANAdapters = Get-WmiObject -Class Win32_NetworkAdapter | Where-Object {($_.Description -notlike “*VirtualBox*”) -and ($_.Description -notlike “*VMware*”) -and ($_.Description -like “*Wireless*”) -or ($_.Description -like “*Wi-Fi*”)}
+    foreach ($WLANAdapter in $WLANAdapters)
+        {
+            #Disable WLAN Adapter
+            $WLANAdapter.Enable()
+        }
+}
+
+
+##################################
+# Function: Test-WiredConnection #
+##################################
+
+function Test-WiredConnection
+{
+#Get only wired connections with IP-address
+$NetworkConnectionsLAN = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter “IPEnabled=TRUE” | Where-Object {($_.Description -notlike “*VirtualBox*”) -and ($_.Description -notlike “*VMware*”) -and ($_.Description -notlike “*Wireless*”) -and ($_.Description -notlike “*Wi-Fi*”)}
+
+If ($NetworkConnectionsLAN -eq $null)
+    {
+        return $false
+    }
+ElseIf ($NetworkConnectionsLAN -ne $null)
+    {
+        return $true
+    }
+}
+
+
+#####################################
+# Function: Test-WirelessConnection #
+#####################################
+
+function Test-WirelessConnection
+{
+#Get only wireless connections with IP-address
+$NetworkConnectionsWLAN = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter “IPEnabled=TRUE” | Where-Object {($_.Description -notlike “*VirtualBox*”) -and ($_.Description -notlike “*VMware*”) -or ($_.Description -like “*Wireless*”) -or ($_.Description -like “*Wi-Fi*”)}
+
+If ($NetworkConnectionsWLAN -eq $null)
+    {
+        return $false
+    }
+ElseIf ($NetworkConnectionsWLAN -ne $null)
+    {
+        return $true
+    }
+}
+
+
+#################################
+# Function: Install-WLANManager #
+#################################
+
+function Install-WLANManager
+{
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$SourcePath,
+
+    [Parameter(Mandatory=$True,Position=2)]
+    [string]$DestinationPath
+)
+
+## Writing Outputs for DEBUG
+## Comment if no DEBUG needed
+
+Write-Output "SourcePath: $SourcePath"
+Write-Output "DestinationPath: $DestinationPath"
+Write-Output "VersionRegPath: $VersionRegPath"
+
+
+    #Write version info to registry for SCCM detection method
+    Write-Output "Verifying WLAN Manager version information... "
+    If ((Test-Path $VersionRegPath) -eq $false)
+        {
+            Write-Output "Missing"
+            Write-Output "Writing WLAN Manager version information... "
+            New-Item -Path $VersionRegPath -Force | Out-Null
+            New-ItemProperty -Path $VersionRegPath -Name Version -PropertyType String -Value $Version | Out-Null
+            Write-Output "Done"
+        }
+    Else
+        {
+            Write-Output "Installed"
+        }
+
+    #Copy Script Files
+    Write-Output "Verify WLAN Manager Files... "
+    If ((Test-Path $DestinationPath) -eq $false)
+        {
+            Write-Output "Missing"
+            Write-Output "Installing WLAN Manager Files... "
+            New-Item -ItemType Directory -Path $DestinationPath -Force | Out-Null
+            Copy-Item -Path "$SourcePath\*" -Destination "$DestinationPath" -Force
+            Write-Output "Done"
+        }
+    Else
+        {
+            Write-Output "Installed"
+        }
+
+    #Register Scheduled Task
+    Write-Output "Verify WLAN Manager Scheduled Task... "
+    #Set correct $TaskName value depending on install for System or User
+    If ($Install -eq "System")
+        {
+            $User = "NT AUTHORITY\System"
+            $TaskName = "WLAN Manager"
+            $TaskRunAt = "8"
+            $TaskRunAs = "System"
+            $TaskRunLevel = "5"
+            #$Trigger
+            If ($Win8orGreater)
+                {
+                    $Trigger = New-ScheduledTaskTrigger -AtStartup
+                }
+            #ReleaseDHCPLease
+            If ($ReleaseDHCPLease -eq "")
+                {
+                    $ReleaseDHCPLease = $false
+                }
+            #BalloonTip
+            $BalloonTip = $false
+        }
+    ElseIf ($Install -eq "User")
+        {
+            $User = "$env:USERNAME"
+            $TaskName = "WLAN Manager ($env:USERNAME)"
+            $TaskRunAt = "8"
+            $TaskRunAs = "User"
+            $TaskRunLevel = "3"
+            #$Trigger
+            If ($Win8orGreater)
+                {
+                    $Trigger = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
+                }
+            #ReleaseDHCPLease
+            If ($ReleaseDHCPLease -eq "")
+                {
+                    $ReleaseDHCPLease = $false
+                }
+            #BalloonTip
+            If ($BalloonTip -eq "")
+                {
+                    $BalloonTip = $false
+                }
+        }
+    #≥Windows 8
+    If ($Win8orGreater)
+        {
+            If ((Get-ScheduledTask -TaskName "$TaskName" -ErrorAction Continue) -like "Not created.")
+                {
+                    Write-Output "Missing"
+                    Write-Output "Installing WLAN Manager Scheduled Task... " 
+                    #$Trigger = New-ScheduledTaskTrigger -AtStartup
+                    $Action = New-ScheduledTaskAction -Execute "$env:windir\System32\WindowsPowerShell\v1.0\PowerShell.exe" -Argument "-WindowStyle Hidden -NonInteractive -Executionpolicy Unrestricted -Command ""& """"$DestinationPath\WLANManager.ps1"""""" -ReleaseDHCPLease:`$$ReleaseDHCPLease -BalloonTip:`$$BalloonTip"""
+                    $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Hidden
+                    Register-ScheduledTask -TaskName "$TaskName" -Trigger $Trigger -User $User –Action $Action -Settings $Settings -RunLevel Highest | Out-Null
+                    Start-ScheduledTask -TaskName "$TaskName"
+                    Write-Output "Done"
+                }
+            Else
+                {
+                    Write-Output "Installed"
+                }
+        }
+    #<Windows 8
+    Else
+        {
+        Write-Output "Taskname: $TaskName"
+            If ((Get-STask -TaskName "$TaskName" -ErrorAction Continue) -like "Not created.")
+                {
+                    Write-Output "Missing"
+                    Write-Output "Installing WLAN Manager Scheduled Task... " 
+                    New-STask -TaskName "$TaskName" -TaskCommand "C:\Windows\System32\WindowsPowerShell\v1.0\PowerShell.exe" -TaskArguments "-WindowStyle Hidden -NonInteractive -Executionpolicy Unrestricted -Command ""& """"$DestinationPath\WLANManager.ps1"""""" -ReleaseDHCPLease:`$$ReleaseDHCPLease -BalloonTip:`$$BalloonTip""" -TaskRunAt $TaskRunAt -TaskRunAs $TaskRunAs -TaskRunLevel $TaskRunLevel| Out-Null
+                    #New-STask -TaskName "$TaskName" -TaskCommand "C:\Windows\System32\WindowsPowerShell\v1.0\PowerShell.exe" -TaskScript "$DestinationPath\WLANManager.ps1" -TaskArguments "-WindowStyle Hidden -NonInteractive -Executionpolicy unrestricted -file ""$DestinationPath\WLANManager.ps1 -BalloonTip:`$$BalloonTip""" | Out-Null
+                    Start-STask -TaskName "$TaskName" | Out-Null
+                    Write-Output "Done"
+                }
+            Else
+                {
+                    Write-Output "Installed"
+                }
+        }
+}
+
+
+################################
+# Function: Remove-WLANManager #
+################################
+
+function Remove-WLANManager
+{
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$FilePath
+)
+    #Set correct $TaskName value depending on install for System or User
+    If ($Remove -eq "System")
+        {
+            $TaskName = "WLAN Manager"
+        }
+    ElseIf ($Remove -eq "User")
+        {
+            $TaskName = "WLAN Manager ($env:USERNAME)"
+        }
+    
+    #Remove version information from registry for SCCM Detection Method
+    Write-Output "Removing WLAN Manager version information... "
+    If ((Test-Path $VersionRegPath) -eq $true)
+        {
+            Remove-Item -Path $VersionRegPath -Force
+            Write-Output "Done"
+        }
+    Else
+        {
+            Write-Output "$VersionRegPath doesn't exist."
+        }
+    
+    #Remove WLAN Manager Scheduled Task
+    Write-Output "Removing WLAN Manager Scheduled Task... "
+    #≥Windows 8
+    If ($Win8orGreater)
+        {
+            If ((Get-ScheduledTask -TaskName "$TaskName" -ErrorAction Continue) -ne $null)
+                {
+                    Unregister-ScheduledTask -TaskName "$TaskName" -Confirm:$false
+                    Write-Output "Done"
+                }
+            Else
+                {
+                    Write-Output "$TaskName doesn't exist."
+                }
+        }
+    #<Windows 8
+    Else
+        {
+            If ((Get-STask -TaskName "$TaskName" -ErrorAction Continue) -ne $null)
+                {
+                    Unregister-STask -TaskName "$TaskName"
+                    Write-Output "Done"
+                }
+            Else
+                {
+                    Write-Output "$TaskName doesn't exist."
+                }
+        }
+    
+    #Remove WLAN Manager files
+    Write-Output "Removing WLAN Manager files... "
+    If ((Test-Path -Path "$FilePath") -eq $True)
+        {
+            Remove-Item -Path $FilePath -Recurse -Confirm:$false
+            Write-Output "Done"
+        }
+    Else
+        {
+            Write-Output "$FilePath doesn't exist."
+        }
+
+    #Verify that WLAN Manager scheduled task object is removed
+    If ((Test-Path -Path "$env:windir\System32\Tasks\$TaskName") -eq $True)
+        {
+            Remove-Item -Path "$env:windir\System32\Tasks\$TaskName" -Confirm:$false
+        }
+}
+
+
+#############
+# New-STask #
+#############
+function New-STask
+{
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$TaskName,
+    [Parameter(Mandatory=$False,Position=2)]
+    [string]$TaskDescription,
+    [Parameter(Mandatory=$True,Position=3)]
+    [string]$TaskCommand,
+    [Parameter(Mandatory=$True,Position=4)]
+    [string]$TaskArguments,
+    [Parameter(Mandatory=$True,Position=5)]
+    [ValidateSet('8', '3')] #8=AtStartup,?=USER
+    [string]$TaskRunAt,
+    [Parameter(Mandatory=$True,Position=6)]
+    #[ValidateSet("System", "User")]
+    [string]$TaskRunAs,
+    [Parameter(Mandatory=$True,Position=7)]
+    [ValidateSet('5', '3')] #5=SYSTEM,3=USER
+    [string]$TaskRunLevel
+)
+#Set correct value for parameter $TaskRunAs
+If ($TaskRunAs -eq "User")
+    {
+        Set-Variable -Name TaskRunAs -Value $env:USERNAME -Force
+    }
+
+#Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
+$Service = New-Object -ComObject("Schedule.Service")
+#Connect to the local machine
+$Service.Connect()
+#Get specified Scheduled Tasks folder
+$TaskFolder = $Service.GetFolder("\")
+
+#Create Task Definition
+$TaskDefinition = $Service.NewTask(0) 
+$TaskDefinition.RegistrationInfo.Description = "$TaskDescription"
+$TaskDefinition.Settings.Enabled = $true
+$TaskDefinition.Settings.AllowDemandStart = $true
+$TaskDefinition.Settings.DisallowStartIfOnBatteries = $false
+$TaskDefinition.Settings.StopIfGoingOnBatteries = $false
+$TaskDefinition.Settings.Compatibility = "3" #Windows 7, Windows Server 2008 R2
+$TaskDefinition.Settings.Hidden = $true
+$TaskDefinition.Principal.RunLevel = "1" #Highest
+
+#Create Task Trigger (http://msdn.microsoft.com/en-us/library/windows/desktop/aa383915(v=vs.85).aspx)
+$Triggers = $TaskDefinition.Triggers
+$Trigger = $Triggers.Create($TaskRunAt)
+$Trigger.Enabled = $true
+ 
+#Create Task Action (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381841(v=vs.85).aspx)
+$Action = $TaskDefinition.Actions.Create(0)
+$Action.Path = "$TaskCommand"
+$Action.Arguments = "$TaskArguments"
+ 
+#Register Task (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381365(v=vs.85).aspx)
+$TaskFolder.RegisterTaskDefinition("$TaskName",$TaskDefinition,6,"$TaskRunAs",$null,$TaskRunLevel)
+#$TaskFolder.RegisterTaskDefinition("$TaskName",$TaskDefinition,6,"$TaskRunAs",$null,5)
+}
+
+
+##############################
+# Function: Unregister-STask #
+##############################
+
+function Unregister-STask
+{
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$TaskName
+)
+#Set ErrorActionPreference to avoid errormessage if TaskName is not found
+$ErrorActionPreference = "SilentlyContinue"
+
+#Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
+$Service = New-Object -ComObject("Schedule.Service")
+#Connect to the local machine
+$Service.Connect()
+#Get specified Scheduled Tasks folder
+$TaskFolder = $Service.GetFolder("\")
+
+#Check if specific task exists and if so delete
+If ($TaskName -eq $TaskFolder.GetTask("$TaskName").Name)
+    {
+        #Delete Task
+        $TaskFolder.DeleteTask("$TaskName",0)
+    }
+
+If(-not $?)
+    {
+        Write-Output "$TaskName not found."
+    }
+}
+
+
+#######################
+# Function: Get-STask #
+#######################
+
+function Get-STask
+{
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$TaskName
+)
+#Set ErrorActionPreference to avoid errormessage if TaskName is not found
+$ErrorActionPreference = "SilentlyContinue"
+
+#Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
+$Service = New-Object -ComObject("Schedule.Service")
+Write-Output "Service: "
+$service
+#Connect to the local machine
+$Service.Connect()
+Write-Output "Service Connected: "
+$service
+#Get specified Scheduled Tasks folder
+$TaskFolder = $Service.GetFolder("\")
+Write-Output "TaskFolder: "
+$TaskFolder
+
+#Check if specific task exists and if so return details
+Write-Output "TaskName: $TaskName"
+try {
+If ($TaskName -eq $TaskFolder.GetTask("$TaskName").Name)
+    {
+        $TaskFolder.GetTask("$TaskName")
+    }
+    
+    }#End try
+catch { Write-Output "Not created."}
+
+<#
+If(-not $?)
+    {
+        Write-Output "$TaskName not found."
+    }
+#>
+}
+
+
+#########################
+# Function: Start-STask #
+#########################
+
+function Start-STask
+{
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$TaskName
+)
+#Set ErrorActionPreference to avoid errormessage if TaskName is not found
+$ErrorActionPreference = "SilentlyContinue"
+
+#Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
+$Service = New-Object -ComObject("Schedule.Service")
+#Connect to the local machine
+$Service.Connect()
+#Get specified Scheduled Tasks folder
+$TaskFolder = $Service.GetFolder("\")
+
+#Check if specific task exists and if so start the task
+If ($TaskName -eq $TaskFolder.GetTask("$TaskName").Name)
+    {
+        $Task = $TaskFolder.GetTask("$TaskName")
+        $Task.Run(1)
+    }
+
+If(-not $?)
+    {
+        Write-Output "$TaskName not found."
+    }
+}
+
+
+############################
+# Function: Show-BallonTip #
+############################
+#Source: http://www.powertheshell.com/balloontip/
+function Show-BalloonTip  
+{
+ 
+  [CmdletBinding(SupportsShouldProcess = $true)]
+  param
+  (
+    [Parameter(Mandatory=$true)]
+    $Text,
+   
+    [Parameter(Mandatory=$true)]
+    $Title,
+   
+    [ValidateSet('None', 'Info', 'Warning', 'Error')]
+    $Icon = 'Info',
+
+    $Timeout = 10000
+  )
+ 
+Add-Type -AssemblyName System.Windows.Forms
+
+if ($global:balloon -eq $null)
+{
+$global:balloon = New-Object System.Windows.Forms.NotifyIcon
+}
+
+$path                    = Get-Process -id $pid | Select-Object -ExpandProperty Path
+$balloon.Icon            = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+$balloon.BalloonTipIcon  = $Icon
+$balloon.BalloonTipText  = $Text
+$balloon.BalloonTipTitle = $Title
+$balloon.Visible         = $true
+
+$balloon.ShowBalloonTip($Timeout)
+} 
+
+
+##############################
+# Function: Remove-BallonTip #
+##############################
+#Source: http://www.powertheshell.com/balloontip/
+function Remove-BalloonTip
+{
+If ($global:balloon -ne $null)
+    {
+        $global:balloon.Dispose()
+        Remove-Variable -Name balloon -Scope Script
+    }
+Else
+    {
+        Write-Warning "Variable 'balloon' not found."
+    }
+}
+
+
+##############################
+# Function: Remove-DHCPLease #
+##############################
+
+function Remove-DHCPLease
+{
+    #Get only WLAN Adapter
+    $WLANAdapterConfiguration = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object {($_.Description -notlike “*VMware*”) -and ($_.Description -like “*Wireless*”) -or ($_.Description -like "*WiFi*") -or ($_.Description -like "*Wi-Fi*")}
+    #Release DHCP lease
+    $WLANAdapterConfiguration.ReleaseDHCPLease() | Out-Null
+}
---
+++ WLANManager.ps1
@@ -0,0 +1,243 @@
+﻿################
+# WLAN Manager #
+################
+#Version: 2015-05-07
+#Author: johan.carlsson@innovatum.se , http://www.innovatum.se/personer/johan-carlsson
+
+## Version 12/03/2018
+## Updated by JENS CHRISTENS - jens.christens@live.com
+
+## Changelog:
+## - Fixed WLANAdapters WMI Query
+## - Fixed WLANAdapters Disable Issues (With Wifi MiniPort)
+## - Updated Write-Hosts for Write-Outputs
+## - Added some troubleshooting hints
+
+<#
+
+.SYNOPSIS
+Disables the WLAN NIC when LAN NIC network connection is verified.
+Enables WLAN NIC when LAN NIC network connection is lost.
+
+.DESCRIPTION
+WLAN Manager runs as a scheduled task and will automatically disable your WLAN card when a LAN connection is verified.
+The WLAN card will be re-enabled once the LAN connection is lost. This ensures you'll always have the fastest available connection and stops network bridging.
+
+.EXAMPLE
+.\WLANManager.ps1
+Runs WLAN Manager in an interactive window. Will not install anything. This mode is only for testing and watching what happens via console output.
+
+.EXAMPLE
+.\WLANManager.ps1 -ReleaseDHCPLease:$true
+Runs WLAN Manager in an interactive window. Will not install anything. This mode is only for testing and watching what happens via console output. Releases DHCP lease before disabling WLAN card.
+
+.EXAMPLE
+.\WLANManager.ps1 -BalloonTip:$true
+Runs WLAN Manager in an interactive window. Will not install anything. This mode is only for testing and watching what happens via console output. Will show a balloontip when enabling or disabling WLAN card.
+
+.EXAMPLE
+.\WLANManager.ps1 -Install:System
+Installs WLAN Manager and will run at all times in the SYSTEM context.
+
+.EXAMPLE
+.\WLANManager.ps1 -Install:System -ReleaseDHCPLease:$true
+Installs WLAN Manager and will run at all times in the SYSTEM context. Releases DHCP lease before disabling WLAN card.
+
+.EXAMPLE
+.\WLANManager.ps1 -Remove:System
+Removes WLAN Manager installation for System.
+
+.EXAMPLE
+.\WLANManager.ps1 -Install:User
+Installs WLAN Manager and will run at user logon in the USER context.
+
+.EXAMPLE
+.\WLANManager.ps1 -Install:User -ReleaseDHCPLease:$true
+Installs WLAN Manager and will run at user logon in the USER context. Releases DHCP lease before disabling WLAN card.
+
+.EXAMPLE
+.\WLANManager.ps1 -Install:User -BalloonTip:$true
+Installs WLAN Manager and will run at user logon in the USER context. Will show a balloontip when enabling or disabling WLAN card.
+
+.EXAMPLE
+.\WLANManager.ps1 -Remove:User
+Removes WLAN Manager installation for User.
+
+.NOTES
+None.
+
+.LINK
+https://gallery.technet.microsoft.com/scriptcenter/WLAN-Manager-f438a4d7
+
+#>
+
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$False,Position=1,HelpMessage="Installs WLAN Manager for System or User.")]
+    [ValidateSet("System", "User")]
+    [string]$Install,
+    [Parameter(Mandatory=$False,Position=2,HelpMessage="Removes WLAN Manager for System or User.")]
+    [ValidateSet("System", "User")]
+    [string]$Remove,
+    [Parameter(Mandatory=$False,Position=3,HelpMessage="Release DHCP lease before disabling WLAN card.")]
+    [switch]$ReleaseDHCPLease,
+    [Parameter(Mandatory=$False,Position=4,HelpMessage="Displays balloontip when changes occur. Only valid when installing or running as User.")]
+    [switch]$BalloonTip
+)
+
+
+#########################################
+# Custom Variables for Your Environment #
+#########################################
+#Destination Path to where you want to store files for local install of WLAN Manager when installing for: System
+$SystemDestinationPath = "$env:ProgramFiles\WLANManager"
+#Registry destination path for writing version information to the registry when installing for: System
+$SystemVersionRegPath = "HKLM:\SOFTWARE\WLAN Manager"
+#Destination Path to where you want to store files for local install of WLAN Manager when installing for: User
+$UserDestinationPath = "$env:LOCALAPPDATA\WLANManager"
+#Registry destination path for writing version information to the registry when installing for: User
+$UserVersionRegPath = "HKCU:\SOFTWARE\WLAN Manager"
+
+
+<#
+D O   N O T   C H A N G E   A N Y T H I N G   B E L O W   T H I S   L I N E
+#>
+
+
+#######################################################
+# Change cmdlet parameters scope from Local to Global #
+#######################################################
+Set-Variable -Name Install -Value $Install -Scope Global -Force
+Set-Variable -Name Remove -Value $Remove -Scope Global -Force
+Set-Variable -Name ReleaseDHCPLease -Value $ReleaseDHCPLease -Scope Global -Force
+Set-Variable -Name BalloonTip -Value $BalloonTip -Scope Global -Force
+
+
+#################################
+# Unload/Load PowerShell Module #
+#################################
+
+#Remove PowerShell Module
+If ((Get-Module PSModule-WLANManager) -ne $null)
+    {
+        Remove-Module PSModule-WLANManager -Verbose
+    }
+
+#Import PowerShell Module
+$strBasePath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Import-Module "$strBasePath\PSModule-WLANManager.psm1" -Verbose
+
+
+#####################
+# Install or Remove #
+#####################
+
+#Version
+$Global:Version = "2015-05-07"
+
+#Set correct Destination and VersionRegPath
+If ($Install -eq "System" -and $BalloonTip -eq $true)
+    {
+        Write-Error -Message "Parameter '-ShowBalloonTip:`$true' is ONLY valid when installing for USER." -Category InvalidArgument -ErrorAction Stop
+    }
+ElseIf ($Install -eq "System" -or $Remove -eq "System")
+    {
+        $CustomDestinationPath = $SystemDestinationPath
+        $Global:VersionRegPath = $SystemVersionRegPath
+    }
+ElseIf ($Install -eq "User" -or $Remove -eq "User")
+    {
+        $CustomDestinationPath = $UserDestinationPath
+        $Global:VersionRegPath = $UserVersionRegPath
+    }
+
+#Install
+If ($Install -ne "")
+    {
+        #Install
+        Install-WLANManager -SourcePath $strBasePath -DestinationPath $CustomDestinationPath
+        Return
+    }
+#Remove
+ElseIf ($Remove -ne "")
+    {
+        Remove-WLANManager -FilePath $CustomDestinationPath
+        Return
+    }
+
+
+########
+# Main #
+########
+
+while ($true)
+{
+    If ((Test-WiredConnection) -eq $true -and (Test-WirelessConnection) -eq $true)
+        {
+            Write-Host "Wired connection detected, disabling Wireless connection... " -NoNewline -ForegroundColor Yellow
+            If ($BalloonTip -eq $true)
+                {
+                    Show-BalloonTip -Text "Wired connection detected, disabling Wireless connection." -Title "WLAN Manager" -Icon Info
+                }
+            If ($ReleaseDHCPLease -eq $true)
+                {
+                    Remove-DHCPLease
+                }
+            #≥Windows 8
+            If ($Win8orGreater)
+                {
+                    $WLANAdapters = Get-NetAdapter -InterfaceDescription *Wireless*,*WiFi*
+                    foreach ($WLANAdapter in $WLANAdapters)
+                        {
+                            Disable-NetAdapter -Name $WLANAdapter.Name -IncludeHidden -Confirm:$False
+                        }
+                }
+            #<Windows 8
+            Else
+                {
+                    Disable-WLANAdapters | Out-Null
+                }
+            #Wait for WLAN to release DHCP lease and get disabled
+            while ((Test-WiredConnection) -eq $true -and (Test-WirelessConnection) -eq $true)
+                {
+                    sleep -Seconds 1
+                }
+            Write-Host "Done" -ForegroundColor White -BackgroundColor Green
+        }
+
+    ElseIf ((Test-WiredConnection) -eq $false -and (Test-WirelessConnection) -eq $false)
+        {
+            Write-Host "Wired connection lost, enabling Wireless connection... " -NoNewline -ForegroundColor Yellow
+            If ($BalloonTip -eq $true)
+                {
+                    Show-BalloonTip -Text "Wired connection lost, enabling Wireless connection." -Title "WLAN Manager" -Icon Info
+                }
+            #≥Windows 8
+            If ($Win8orGreater)
+                {
+                    $WLANAdapters = Get-NetAdapter -InterfaceDescription *Wireless*,*WiFi*
+                    foreach ($WLANAdapter in $WLANAdapters)
+                        {
+                            Enable-NetAdapter -Name $WLANAdapter.Name -IncludeHidden -Confirm:$False
+                        }
+                }
+            #<Windows 8
+            Else
+                {
+                    Enable-WLANAdapters | Out-Null
+                }
+            #Wait for WLAN Adapter to initialize and obtain an IP-address
+            while ((Test-WiredConnection) -eq $false -and (Test-WirelessConnection) -eq $false)
+                {
+                    sleep -Seconds 1
+                }
+            Write-Host "Done" -ForegroundColor White -BackgroundColor Green
+        }
+
+    Else
+        {
+            Write-Host "Sleeping..." -ForegroundColor Yellow
+            sleep -Seconds 1
+        }
+}

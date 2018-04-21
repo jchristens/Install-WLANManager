34c9f81f073b6d4afedd30aa8840387fbe87af3a
jchristens <jens.christens@live.com>
2018-04-21 11:06:36 +0200

    Update PSModule-WLANManager.psm1
    Edited documentation

--- PSModule-WLANManager.psm1
+++ PSModule-WLANManager.psm1
@@ -1,61 +1,71 @@
+<#
 ################
 # WLAN Manager #
 ################
-#Version: 2015-05-07
-#Author: johan.carlsson@innovatum.se , http://www.innovatum.se/personer/johan-carlsson
 
-## Version 12/03/2018
-## Updated by JENS CHRISTENS - jens.christens@live.com
+.GUID
+b53c75e4-3ef4-418a-9405-7537942afbb6
 
-## Changelog:
-## - Fixed create Schedule Task issue (Exception calling "GetTask" with "1" argument(s): "The system cannot find the file specified. (Exception from HRESULT: 0x80070002)
-## - Updated WLANAdapters WMI Query
-## - Updated WLANAdapters Disable Issues (With Wifi MiniPort)
-## - Updated Write-Hosts for Write-Outputs
-## - Added some troubleshooting hints
+.VERSION
+2015-05-07
 
-#Collect OS Information
+.AUTHOR 
+johan.carlsson@innovatum.se
+http://www.innovatum.se/personer/johan-carlsson
+
+.UPDATE
+12/03/2018
+Updated by JENS CHRISTENS
+jens.christens@live.com
+
+.CHANGELOG
+- Fixed create Schedule Task issue (Exception calling "GetTask" with "1" argument(s): "The system cannot find the file specified. (Exception from HRESULT: 0x80070002)
+- Updated WLANAdapters WMI Query
+- Updated WLANAdapters Disable Issues (With Wifi MiniPort)
+- Updated Write-Hosts for Write-Outputs
+- Added some troubleshooting hints
+- Fixed the Win8 identification
+
+#>
+
+## Collect OS Information
+
 $Global:Win8orGreater = [Environment]::OSVersion.Version -ge (new-object 'Version' 8,0)
 
-##################################
-# Function: Disable-WLANAdapters #
-##################################
+## Function: Disable-WLANAdapters 
 
 function Disable-WLANAdapters
 {
-    #Get only WLAN Adapters
+    ## Get only WLAN Adapters - possibly needs to be changed depending on environment
     $WLANAdapters = Get-WmiObject -Class Win32_NetworkAdapter | Where-Object {($_.Description -notlike “*VirtualBox*”) -and ($_.Description -notlike “*VMware*”) -and ($_.Description -like “*Wireless*”) -or ($_.Description -like “*Wi-Fi*”)}
     foreach ($WLANAdapter in $WLANAdapters)
         {
-            #Disable WLAN Adapter
+            ## Disable WLAN Adapter
             $WLANAdapter.Disable()
         }
 }
 
 
-#################################
-# Function: Enable-WLANAdapters #
-#################################
+
+## Function: Enable-WLANAdapters
 
 function Enable-WLANAdapters
 {
-    #Get only WLAN Adapter
+    ## Get only WLAN Adapter
     $WLANAdapters = Get-WmiObject -Class Win32_NetworkAdapter | Where-Object {($_.Description -notlike “*VirtualBox*”) -and ($_.Description -notlike “*VMware*”) -and ($_.Description -like “*Wireless*”) -or ($_.Description -like “*Wi-Fi*”)}
     foreach ($WLANAdapter in $WLANAdapters)
         {
-            #Disable WLAN Adapter
+            ## Disable WLAN Adapter
             $WLANAdapter.Enable()
         }
 }
 
 
-##################################
-# Function: Test-WiredConnection #
-##################################
+## Function: Test-WiredConnection 
 
 function Test-WiredConnection
 {
-#Get only wired connections with IP-address
+## Get only wired connections with IP-address
 $NetworkConnectionsLAN = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter “IPEnabled=TRUE” | Where-Object {($_.Description -notlike “*VirtualBox*”) -and ($_.Description -notlike “*VMware*”) -and ($_.Description -notlike “*Wireless*”) -and ($_.Description -notlike “*Wi-Fi*”)}
 
 If ($NetworkConnectionsLAN -eq $null)
@@ -69,13 +79,13 @@ ElseIf ($NetworkConnectionsLAN -ne $null)
 }
 
 
-#####################################
-# Function: Test-WirelessConnection #
-#####################################
+
+## Function: Test-WirelessConnection 
+
 
 function Test-WirelessConnection
 {
-#Get only wireless connections with IP-address
+## Get only wireless connections with IP-address
 $NetworkConnectionsWLAN = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter “IPEnabled=TRUE” | Where-Object {($_.Description -notlike “*VirtualBox*”) -and ($_.Description -notlike “*VMware*”) -or ($_.Description -like “*Wireless*”) -or ($_.Description -like “*Wi-Fi*”)}
 
 If ($NetworkConnectionsWLAN -eq $null)
@@ -89,9 +99,8 @@ ElseIf ($NetworkConnectionsWLAN -ne $null)
 }
 
 
-#################################
-# Function: Install-WLANManager #
-#################################
+
+## Function: Install-WLANManager 
 
 function Install-WLANManager
 {
@@ -113,7 +122,7 @@ Write-Output "DestinationPath: $DestinationPath"
 Write-Output "VersionRegPath: $VersionRegPath"
 
 
-    #Write version info to registry for SCCM detection method
+    ## Write version info to registry for SCCM detection method
     Write-Output "Verifying WLAN Manager version information... "
     If ((Test-Path $VersionRegPath) -eq $false)
         {
@@ -128,7 +137,7 @@ Write-Output "VersionRegPath: $VersionRegPath"
             Write-Output "Installed"
         }
 
-    #Copy Script Files
+    ## Copy Script Files
     Write-Output "Verify WLAN Manager Files... "
     If ((Test-Path $DestinationPath) -eq $false)
         {
@@ -143,9 +152,10 @@ Write-Output "VersionRegPath: $VersionRegPath"
             Write-Output "Installed"
         }
 
-    #Register Scheduled Task
+    ## Register Scheduled Task
     Write-Output "Verify WLAN Manager Scheduled Task... "
-    #Set correct $TaskName value depending on install for System or User
+
+    ## Set correct $TaskName value depending on install for System or User
     If ($Install -eq "System")
         {
             $User = "NT AUTHORITY\System"
@@ -153,17 +163,17 @@ Write-Output "VersionRegPath: $VersionRegPath"
             $TaskRunAt = "8"
             $TaskRunAs = "System"
             $TaskRunLevel = "5"
-            #$Trigger
+            ## $Trigger
             If ($Win8orGreater)
                 {
                     $Trigger = New-ScheduledTaskTrigger -AtStartup
                 }
-            #ReleaseDHCPLease
+            ## ReleaseDHCPLease
             If ($ReleaseDHCPLease -eq "")
                 {
                     $ReleaseDHCPLease = $false
                 }
-            #BalloonTip
+            ## BalloonTip
             $BalloonTip = $false
         }
     ElseIf ($Install -eq "User")
@@ -173,23 +183,24 @@ Write-Output "VersionRegPath: $VersionRegPath"
             $TaskRunAt = "8"
             $TaskRunAs = "User"
             $TaskRunLevel = "3"
-            #$Trigger
+            ## $Trigger
             If ($Win8orGreater)
                 {
                     $Trigger = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
                 }
-            #ReleaseDHCPLease
+            ## ReleaseDHCPLease
             If ($ReleaseDHCPLease -eq "")
                 {
                     $ReleaseDHCPLease = $false
                 }
-            #BalloonTip
+            ## BalloonTip
             If ($BalloonTip -eq "")
                 {
                     $BalloonTip = $false
                 }
         }
-    #≥Windows 8
+    ## ≥Windows 8
+
     If ($Win8orGreater)
         {
             If ((Get-ScheduledTask -TaskName "$TaskName" -ErrorAction Continue) -like "Not created.")
@@ -202,13 +213,14 @@ Write-Output "VersionRegPath: $VersionRegPath"
                     Register-ScheduledTask -TaskName "$TaskName" -Trigger $Trigger -User $User –Action $Action -Settings $Settings -RunLevel Highest | Out-Null
                     Start-ScheduledTask -TaskName "$TaskName"
                     Write-Output "Done"
-                }
+                }#End if get schtask not created
             Else
                 {
                     Write-Output "Installed"
-                }
-        }
-    #<Windows 8
+                }#End else
+        }#End if win8orgreater
+    ## <Windows 8
+
     Else
         {
         Write-Output "Taskname: $TaskName"
@@ -220,18 +232,16 @@ Write-Output "VersionRegPath: $VersionRegPath"
                     #New-STask -TaskName "$TaskName" -TaskCommand "C:\Windows\System32\WindowsPowerShell\v1.0\PowerShell.exe" -TaskScript "$DestinationPath\WLANManager.ps1" -TaskArguments "-WindowStyle Hidden -NonInteractive -Executionpolicy unrestricted -file ""$DestinationPath\WLANManager.ps1 -BalloonTip:`$$BalloonTip""" | Out-Null
                     Start-STask -TaskName "$TaskName" | Out-Null
                     Write-Output "Done"
-                }
+                }#End if get-stask not created
             Else
                 {
                     Write-Output "Installed"
-                }
-        }
+                }#End else
+        }#End else win8 or greater
 }
 
 
-################################
-# Function: Remove-WLANManager #
-################################
+## Function: Remove-WLANManager
 
 function Remove-WLANManager
 {
@@ -241,29 +251,29 @@ Param
     [Parameter(Mandatory=$True,Position=1)]
     [string]$FilePath
 )
-    #Set correct $TaskName value depending on install for System or User
+    ## Set correct $TaskName value depending on install for System or User
     If ($Remove -eq "System")
         {
             $TaskName = "WLAN Manager"
-        }
+        }#End if remove eq system
     ElseIf ($Remove -eq "User")
         {
             $TaskName = "WLAN Manager ($env:USERNAME)"
-        }
+        }#End elseif remove eq user
     
-    #Remove version information from registry for SCCM Detection Method
+    ## Remove version information from registry for SCCM Detection Method
     Write-Output "Removing WLAN Manager version information... "
     If ((Test-Path $VersionRegPath) -eq $true)
         {
             Remove-Item -Path $VersionRegPath -Force
             Write-Output "Done"
-        }
+        }#End if test-path versionregpath eq true
     Else
         {
             Write-Output "$VersionRegPath doesn't exist."
-        }
+        }#End else
     
-    #Remove WLAN Manager Scheduled Task
+    ## Remove WLAN Manager Scheduled Task
     Write-Output "Removing WLAN Manager Scheduled Task... "
     #≥Windows 8
     If ($Win8orGreater)
@@ -272,11 +282,11 @@ Param
                 {
                     Unregister-ScheduledTask -TaskName "$TaskName" -Confirm:$false
                     Write-Output "Done"
-                }
+                }#End if get-schtask -ne null
             Else
                 {
                     Write-Output "$TaskName doesn't exist."
-                }
+                }#End else 
         }
     #<Windows 8
     Else
@@ -285,36 +295,37 @@ Param
                 {
                     Unregister-STask -TaskName "$TaskName"
                     Write-Output "Done"
-                }
+                }#End if get-stask -ne null
             Else
                 {
                     Write-Output "$TaskName doesn't exist."
-                }
-        }
+                }#End else
+        }#End else greater win8
     
-    #Remove WLAN Manager files
+    ## Remove WLAN Manager files
+
     Write-Output "Removing WLAN Manager files... "
     If ((Test-Path -Path "$FilePath") -eq $True)
         {
             Remove-Item -Path $FilePath -Recurse -Confirm:$false
             Write-Output "Done"
-        }
+        }#End if test-path eq true
     Else
         {
             Write-Output "$FilePath doesn't exist."
-        }
+        }#End else
 
     #Verify that WLAN Manager scheduled task object is removed
     If ((Test-Path -Path "$env:windir\System32\Tasks\$TaskName") -eq $True)
         {
             Remove-Item -Path "$env:windir\System32\Tasks\$TaskName" -Confirm:$false
-        }
+        }#End if test-path eq true
 }
 
 
-#############
-# New-STask #
-#############
+
+## Function: New-STask 
+
 function New-STask
 {
 [CmdletBinding()]
@@ -338,20 +349,23 @@ Param
     [ValidateSet('5', '3')] #5=SYSTEM,3=USER
     [string]$TaskRunLevel
 )
-#Set correct value for parameter $TaskRunAs
+## Set correct value for parameter $TaskRunAs
+
 If ($TaskRunAs -eq "User")
     {
         Set-Variable -Name TaskRunAs -Value $env:USERNAME -Force
     }
 
-#Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
+## Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
 $Service = New-Object -ComObject("Schedule.Service")
-#Connect to the local machine
+
+## Connect to the local machine
 $Service.Connect()
-#Get specified Scheduled Tasks folder
+
+##Get specified Scheduled Tasks folder
 $TaskFolder = $Service.GetFolder("\")
 
-#Create Task Definition
+## Create Task Definition
 $TaskDefinition = $Service.NewTask(0) 
 $TaskDefinition.RegistrationInfo.Description = "$TaskDescription"
 $TaskDefinition.Settings.Enabled = $true
@@ -362,25 +376,22 @@ $TaskDefinition.Settings.Compatibility = "3" #Windows 7, Windows Server 2008 R2
 $TaskDefinition.Settings.Hidden = $true
 $TaskDefinition.Principal.RunLevel = "1" #Highest
 
-#Create Task Trigger (http://msdn.microsoft.com/en-us/library/windows/desktop/aa383915(v=vs.85).aspx)
+## Create Task Trigger (http://msdn.microsoft.com/en-us/library/windows/desktop/aa383915(v=vs.85).aspx)
 $Triggers = $TaskDefinition.Triggers
 $Trigger = $Triggers.Create($TaskRunAt)
 $Trigger.Enabled = $true
  
-#Create Task Action (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381841(v=vs.85).aspx)
+## Create Task Action (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381841(v=vs.85).aspx)
 $Action = $TaskDefinition.Actions.Create(0)
 $Action.Path = "$TaskCommand"
 $Action.Arguments = "$TaskArguments"
  
-#Register Task (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381365(v=vs.85).aspx)
+## Register Task (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381365(v=vs.85).aspx)
 $TaskFolder.RegisterTaskDefinition("$TaskName",$TaskDefinition,6,"$TaskRunAs",$null,$TaskRunLevel)
 #$TaskFolder.RegisterTaskDefinition("$TaskName",$TaskDefinition,6,"$TaskRunAs",$null,5)
 }
 
-
-##############################
-# Function: Unregister-STask #
-##############################
+## Function: Unregister-STask
 
 function Unregister-STask
 {
@@ -390,33 +401,35 @@ Param
     [Parameter(Mandatory=$True,Position=1)]
     [string]$TaskName
 )
-#Set ErrorActionPreference to avoid errormessage if TaskName is not found
+
+## Set ErrorActionPreference to avoid errormessage if TaskName is not found
 $ErrorActionPreference = "SilentlyContinue"
 
-#Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
+## Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
 $Service = New-Object -ComObject("Schedule.Service")
-#Connect to the local machine
+
+## Connect to the local machine
 $Service.Connect()
-#Get specified Scheduled Tasks folder
+
+## Get specified Scheduled Tasks folder
 $TaskFolder = $Service.GetFolder("\")
 
-#Check if specific task exists and if so delete
+## Check if specific task exists and if so delete
 If ($TaskName -eq $TaskFolder.GetTask("$TaskName").Name)
     {
         #Delete Task
         $TaskFolder.DeleteTask("$TaskName",0)
-    }
+    }#ENd if taskname eq taskfolder.gettask.name
 
 If(-not $?)
     {
         Write-Output "$TaskName not found."
-    }
+    }#End if not true
 }
 
 
-#######################
-# Function: Get-STask #
-#######################
+
+## Function: Get-STask
 
 function Get-STask
 {
@@ -426,29 +439,32 @@ Param
     [Parameter(Mandatory=$True,Position=1)]
     [string]$TaskName
 )
-#Set ErrorActionPreference to avoid errormessage if TaskName is not found
+
+## Set ErrorActionPreference to avoid errormessage if TaskName is not found
 $ErrorActionPreference = "SilentlyContinue"
 
-#Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
+## Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
 $Service = New-Object -ComObject("Schedule.Service")
 Write-Output "Service: "
 $service
-#Connect to the local machine
+
+## Connect to the local machine
 $Service.Connect()
 Write-Output "Service Connected: "
 $service
-#Get specified Scheduled Tasks folder
+
+## Get specified Scheduled Tasks folder
 $TaskFolder = $Service.GetFolder("\")
 Write-Output "TaskFolder: "
 $TaskFolder
 
-#Check if specific task exists and if so return details
+## Check if specific task exists and if so return details
 Write-Output "TaskName: $TaskName"
 try {
 If ($TaskName -eq $TaskFolder.GetTask("$TaskName").Name)
     {
         $TaskFolder.GetTask("$TaskName")
-    }
+    }#End if taskname eq taskfolder.gettask.taskname
     
     }#End try
 catch { Write-Output "Not created."}
@@ -457,14 +473,13 @@ catch { Write-Output "Not created."}
 If(-not $?)
     {
         Write-Output "$TaskName not found."
-    }
+    }#End if not true
 #>
 }
 
 
-#########################
-# Function: Start-STask #
-#########################
+
+## Function: Start-STask
 
 function Start-STask
 {
@@ -474,34 +489,37 @@ Param
     [Parameter(Mandatory=$True,Position=1)]
     [string]$TaskName
 )
-#Set ErrorActionPreference to avoid errormessage if TaskName is not found
+
+## Set ErrorActionPreference to avoid errormessage if TaskName is not found
 $ErrorActionPreference = "SilentlyContinue"
 
-#Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
+## Create Task Scheduler Com Object (http://msdn.microsoft.com/en-us/library/windows/desktop/aa381833(v=vs.85).aspx)
 $Service = New-Object -ComObject("Schedule.Service")
-#Connect to the local machine
+
+## Connect to the local machine
 $Service.Connect()
-#Get specified Scheduled Tasks folder
+
+## Get specified Scheduled Tasks folder
 $TaskFolder = $Service.GetFolder("\")
 
-#Check if specific task exists and if so start the task
+## Check if specific task exists and if so start the task
 If ($TaskName -eq $TaskFolder.GetTask("$TaskName").Name)
     {
         $Task = $TaskFolder.GetTask("$TaskName")
         $Task.Run(1)
-    }
+    }#End if taskname eq taskfolder.gettask.taskname
 
 If(-not $?)
     {
         Write-Output "$TaskName not found."
-    }
+    }#End if not true
 }
 
 
-############################
-# Function: Show-BallonTip #
-############################
-#Source: http://www.powertheshell.com/balloontip/
+
+## Function: Show-BallonTip 
+## Source: http://www.powertheshell.com/balloontip/
+
 function Show-BalloonTip  
 {
  
@@ -538,10 +556,10 @@ $balloon.ShowBalloonTip($Timeout)
 } 
 
 
-##############################
-# Function: Remove-BallonTip #
-##############################
-#Source: http://www.powertheshell.com/balloontip/
+
+## Function: Remove-BallonTip 
+## Source: http://www.powertheshell.com/balloontip/
+
 function Remove-BalloonTip
 {
 If ($global:balloon -ne $null)
@@ -556,9 +574,8 @@ Else
 }
 
 
-##############################
-# Function: Remove-DHCPLease #
-##############################
+
+## Function: Remove-DHCPLease 
 
 function Remove-DHCPLease
 {
